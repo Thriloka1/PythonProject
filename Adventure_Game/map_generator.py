5fb351c7ce216f7a1e44a84ff7d81555ded72dba
M.Thriloka <145680704+Thriloka1@users.noreply.github.com>
2025-09-24 08:44:58 +0530

    Delete Adventure_Game directory

--- Adventure_Game/map_generator.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def generate_map(size):
-    cell_types = ["Empty", "Enemy", "Item", "Trap"]
-    return [[{"type": random.choices(cell_types, weights=[50,20,20,10])[0], "visited": False}
-             for _ in range(size)] for _ in range(size)]
